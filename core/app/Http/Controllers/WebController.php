b7f26cce36039214279500f6154194986a1f361e
malik2341971-debug <malik2341971@gmail.com>
2026-08-22 06:32:40 -0700

    feat: backend architecture, routing, controllers and configuration

---
+++ core/app/Http/Controllers/WebController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccountListing;
+use App\Models\BiddingListing;
+
+class WebController extends Controller
+{
+    public function accountListing()
+    {
+        return to_route('buy.account');
+    }
+
+    public function accountListingDetails($slug, $id)
+    {
+        $pageTitle      = 'Account Listing Details';
+        $accountListing = AccountListing::active()->activeSocialMedia()->activeCategory()->checkPreviousDate()->withMax('accountBidding', 'amount')->findOrFail($id);
+        $biddings       = BiddingListing::with('user')->where('account_listing_id', $accountListing->id)->orderBy('amount', 'desc')->get();
+        $myBid          = BiddingListing::where('user_id', auth()->id())->where('account_listing_id', $accountListing->id)->first();
+        
+        $relatedAccounts = AccountListing::active()
+            ->withCount('accountBidding')
+            ->withMax('accountBidding', 'amount')
+            ->where('id', '!=', $accountListing->id)
+            ->myBidCount()
+            ->MyBid()
+            ->checkPreviousDate()
+            ->where('category_id', $accountListing->category_id)
+            ->get();
+
+        $seoContents['keywords']           = [@$accountListing->category->name, @$accountListing->socialMedia->name];
+        $seoContents['social_title']       = $accountListing->title;
+        $seoContents['description']        = strLimit(strip_tags($accountListing->description), 150);
+        $seoContents['social_description'] = strLimit(strip_tags($accountListing->description), 150);
+        $seoContents['image']              = getImage(getFilePath('account_listing_thumb') . '/' . $accountListing->thumbnail_image, '470x275');
+        $seoContents['image_size']         = '470x275';
+
+        return view('Template::account_listings_details', compact('pageTitle', 'accountListing', 'biddings', 'relatedAccounts', 'seoContents', 'myBid'));
+    }
+}
